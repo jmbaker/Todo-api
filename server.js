19647da8f0f769c9cb058a38cefa4e0047223088
Jeremy <user.email>
2016-06-07 21:46:02 -0500

    Add POST for todos

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 var PORT = process.env.PORT || 3000;
-var todos = [{
-	id: 1,
-	description: 'Meet mom for lunch',
-	completed: false
-}, {
-	id: 2,
-	description: 'Go to market',
-	completed: false
-}, {
-	id: 3,
-	description: 'Walk the dog',
-	completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
 	res.send('Todo API root');
@@ -45,6 +37,18 @@ app.get('/todos/:id', function(req, res) {
 	
 	
 
+});
+
+// POST /todos
+app.post('/todos', function(req, res) {
+	var body = req.body;
+
+	body.id = todoNextId++;
+	
+	todos.push(body);
+	// push body into array
+
+	res.json(body);
 });
 
 app.listen(PORT, function() {
