96f9b77f2d2ed3d0b974e539cfcc7bb17407db50
Jeremy <user.email>
2016-06-08 19:06:52 -0500

    Final static todos

--- variable-playground.js
+++ variable-playground.js
@@ -1,11 +1,24 @@
-var person = {
-	name: 'Andrew',
-	age: 21
-};
+var _ = require("underscore");
 
-function updatePerson(obj) {
-	obj = {
-		name: "Andrew",
-		age: 24
-	};
-}
+var objs = [{
+	"completed": true,
+	"description": "Walk the dog"	
+},{
+	"completed": false,
+	"description": "Build the fence"
+}, {
+	"completed": false,
+	"description": "Feed the dog"
+}, {
+	"completed": true,
+	"description": "Run the mill"
+}];
+
+
+
+var evens = _.filter(objs, function(obj){
+	console.log(obj.description);
+	return obj.description.indexOf("dog") > -1;
+});
+
+console.log(evens);
