9e9e6db90e4aa4d33d546ee6e41928cf42657b90
Jeremy <user.email>
2016-06-15 19:38:36 -0500

    Add auth middleware and findByToken class method

--- models/user.js
+++ models/user.js
@@ -62,6 +62,31 @@ module.exports = function(sequelize, DataTypes) {
 						return reject();
 					});
 				});
+			},
+			findByToken: function(token) {
+				return new Promise(function(resolve, reject) {
+					try {
+						var decodedJWT = jwt.verify(token, 'qwerty098');
+						var bytes = cryptojs.AES.decrypt(decodedJWT.token, 'abc123!@#');
+						var tokenData = JSON.parse(bytes.toString(cryptojs.enc.Utf8));
+
+						user.findById(tokenData.id).then(function(user) {
+							if(user) {
+								resolve(user);
+							} else {
+								console.log('rejected in else');
+								reject();
+							}
+						}, function(e) {
+							console.log('rejected in find by id');
+							reject();
+						});
+					} catch(e) {
+						console.log(e);
+						reject();
+					}
+				});
+
 			}
 		},
 		instanceMethods: {
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 var _ = require("underscore");
 var db = require('./db.js');
 var bcrypt = require('bcrypt');
+var middleware = require('./middleware.js')(db);
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -16,7 +17,7 @@ app.get('/', function(req, res) {
 });
 
 // GET /todos?completed=true&q=example
-app.get('/todos', function(req, res) {
+app.get('/todos', middleware.requireAuthentication, function(req, res) {
 	var query = req.query;
 	var where = {};
 
@@ -43,27 +44,10 @@ app.get('/todos', function(req, res) {
 	}, function(e) {
 		res.status(500).send();
 	});
-	// var filteredTodos = todos;
-
-	// if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-	// 	filteredTodos = _.where(filteredTodos, {"completed" : true});
-	// } else if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-	// 	filteredTodos = _.where(filteredTodos, {"completed" : false});
-	// }
-
-	
-
-	// if(queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-	// 	filteredTodos = _.filter(filteredTodos, function(todo) {
-	// 		return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
-	// 	});
-	// }
-
-	// res.json(filteredTodos);
 });
 
 // GET /todos/:id
-app.get('/todos/:id', function(req, res) {
+app.get('/todos/:id', middleware.requireAuthentication, function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 
 	db.todo.findById(todoId).then(function(todo) {
@@ -75,55 +59,22 @@ app.get('/todos/:id', function(req, res) {
 	}, function(e) {
 		res.status(500).send();
 	});
-
-
-	// var matchedTodo = _.findWhere(todos, {id: todoId});
-
-	// if(matchedTodo) {
-	// 	res.json(matchedTodo);
-	// } else {
-	// 	res.status(404).send();	
-	// }
-
 });
 
 // POST /todos
-app.post('/todos', function(req, res) {
+app.post('/todos', middleware.requireAuthentication, function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
-
-	// if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-	// 	return res.status(400).send();
-	// }
-	// body.description = body.description.trim();
-	// body.id = todoNextId++;
-
-	// db.todo.create({
-	// 	description: body.description,
-	// 	completed: body.completed,
-	// 	id: body.id
-	// }).then(function(body) {
-	// 	res.json(body);
-	// }).catch(function(e) {
-	// 	res.status(400).send(e);
-	// });
 	
-
 	db.todo.create(body).then(function(todo) {
-		res.json(todo.toPublicJSON());
+		res.json(todo.toJSON());
 	}, function(e) {
 		res.status(400).json(e);
 	});
-
-	
-	// body.id = todoNextId++;
-	// // push body into array
-
-	// res.json(body);
 });
 
 // DELETE /todos/:id
 
-app.delete('/todos/:id', function(req, res) {
+app.delete('/todos/:id', middleware.requireAuthentication, function(req, res) {
 
 	var todoId = parseInt(req.params.id, 10);
 
@@ -142,19 +93,10 @@ app.delete('/todos/:id', function(req, res) {
 		res.status(500).send();
 	});
 	
-	// var matchedTodo = _.findWhere(todos, {id: todoId});
-
-	// if(!matchedTodo) {
-	// 	res.status(404).json({"error": "no todo found with that id"});
-	// } else {
-	// 	todos = _.without(todos, matchedTodo);
-	// 	res.json(matchedTodo);	
-	// }
-	
 });
 
 // PUT /todos/:id
-app.put('/todos/:id', function(req, res) {
+app.put('/todos/:id', middleware.requireAuthentication, function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
 	
 
