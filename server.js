eb8f7926e8ceefa5d93ab6df910d4d225439d9d5
Jeremy <user.email>
2016-06-22 22:46:23 -0500

    Add DELETE /users/login

--- server.js
+++ server.js
@@ -153,9 +153,15 @@ app.post('/users', function(req, res) {
 // POST /users/login
 app.post('/users/login', function(req, res) {
 	var body = _.pick(req.body, 'email', 'password');
+	var userInstance;
 
 	db.user.authenticate(body).then(function(user) {
 		var token = user.generateToken('authentication');
+		userInstance = user;
+
+		return db.token.create({
+			token: token
+		});
 		
 		if(token) {
 			res.header('Auth', token).json(user.toPublicJSON());
@@ -163,11 +169,22 @@ app.post('/users/login', function(req, res) {
 			res.status(401).send();	
 		}
 
-	}, function() {
+	}).then(function(tokenInstance) {
+		res.header('Auth', tokenInstance.get('token')).json(userInstance.toPublicJSON());
+	}).catch(function() {
 		res.status(401).send();
 	});
 });
 
+
+// DELETE /users/login
+app.delete('/users/login', middleware.requireAuthentication, function(req, res) {
+	req.token.destroy().then(function() {
+		res.status(204).send();
+	}).catch(function() {
+		res.status(500).send();
+	});
+});
 
 db.sequelize.sync({force: true}).then(function() {
 	app.listen(PORT, function() {
